babb6de3d21013ab04eefb49d266b451b4b618e9
Victoria Ternovaya <victoria.ternovaya@gmail.com>
2025-03-24 18:45:14 +0300

    feat: complete getDayName, getNextFriday and getCountDaysInMonth tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -49,8 +49,19 @@ function getTime(date) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const dateObj = new Date(date);
+  const dayIndex = dateObj.getUTCDay();
+  return days[dayIndex];
 }
 
 /**
@@ -64,8 +75,20 @@ function getDayName(/* date */) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const dateCopy = new Date(date.getTime());
+  const currentDay = dateCopy.getUTCDay();
+  let daysToAdd;
+  if (currentDay <= 5) {
+    daysToAdd = 5 - currentDay;
+  } else {
+    daysToAdd = 6;
+  }
+  if (currentDay === 5) {
+    daysToAdd = 7;
+  }
+  dateCopy.setUTCDate(dateCopy.getUTCDate() + daysToAdd);
+  return dateCopy;
 }
 
 /**
@@ -79,8 +102,9 @@ function getNextFriday(/* date */) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  const lastDay = new Date(year, month, 0);
+  return lastDay.getDate();
 }
 
 /**
